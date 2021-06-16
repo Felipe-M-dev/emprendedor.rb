c40569bd3c7d70bfa37cfc30b544777b3d353bc6
Felipe-M-dev <39914824+Felipe-M-dev@users.noreply.github.com>
2021-06-16 18:40:13 -0400

    Update emprendedor2.rb
    Add order and fix input

--- emprendedor2.rb
+++ emprendedor2.rb
@@ -1,31 +1,36 @@
 #Inicio
 #Ingrese el precio
-#Ingrese el número de usuarios normales
+#Ingrese el total de usuarios
 #Ingrese el número de usuarios premium
 #Ingrese el número de usuarios gratuitos
 #Ingrese los gastos
+#Calcula usuarios normales = usuarios premium - usuarios gratuitos
 #Utilidades positivas mostrar utilidades * 0.65
 #Utilidades negativas mostras utilidades
 #Fin
 
 sale_price = ARGV[0].to_i
-users_normal = ARGV[1].to_i
+users = ARGV[1].to_i
 users_premium = ARGV[2].to_i
 users_free = ARGV[3].to_i
+users_normal = (users - users_premium - users_free)
 expenses = ARGV[4].to_i
 tax = 0.65
 utility = (sale_price * (users_normal + (users_premium * 2) + (users_free * 0)) - expenses)
 
-puts " ------------------------------------"
-puts "|Precio de venta = #{sale_price}.               |"
-puts "|Cantidad de usuarios normales = #{users_normal}.|"
-puts "|Cantidad de usuarios premium  = #{users_premium}.|"
-puts "|Cantidad de usuarios gratis   = #{users_free}.|"
-puts "|Gastos = #{expenses}.                     |"
-puts " ------------------------------------"
+puts " -------------------------------------"
+puts "Precio de venta = #{sale_price}."
+puts "Total de usuarios             = #{users}."
+puts "Cantidad de usuarios normales = #{users_normal}."
+puts "Cantidad de usuarios premium  = #{users_premium}."
+puts "Cantidad de usuarios gratis   = #{users_free}."
+puts "Gastos = #{expenses}."
+puts " -------------------------------------"
 
 if utility > 0
   puts "Las utilidades son de: #{utility*tax}"
+  puts " -------------------------------------"
 else
   puts "Las utilidades son de: #{utility}"
+  puts " -------------------------------------"
 end
